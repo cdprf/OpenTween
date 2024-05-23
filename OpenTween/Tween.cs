cdprf/OpenTween

commit f2ae9689fbe063855fe6f7ece7ee20bed843a35f

MyCommon.TwitterRateLimitsを廃止しアカウントごとにレートリミットを把握できるようにする

OpenTween/Tween.cs
@@ -466,7 +466,7 @@ namespace OpenTween
             // タブの位置を調整する
             this.SetTabAlignment();
 
-            this.unsubscribeRateLimitUpdate = MyCommon.TwitterRateLimits.SubscribeAccessLimitUpdated(this.TwitterApiStatus_AccessLimitUpdated);
+            this.SubscribePrimaryAccountRatelimit();
 
             Microsoft.Win32.SystemEvents.TimeChanged += this.SystemEvents_TimeChanged;
 
@@ -2614,7 +2614,10 @@ namespace OpenTween
             this.SaveConfigsAll(false);
 
             if (this.PrimaryAccount.UniqueKey != previousAccountId)
+            {
+                this.SubscribePrimaryAccountRatelimit();
                 await this.RefreshConfigurationAsync();
+            }
 
             var currentSecondaryAccounts = this.accounts.SecondaryAccounts;
             var newSecondaryAccounts = currentSecondaryAccounts
@@ -3056,7 +3059,7 @@ namespace OpenTween
         {
             this.SetMainWindowTitle();
             this.SetStatusLabelUrl();
-            this.SetApiStatusLabel();
+            this.SetApiStatusLabel(null);
             if (this.ListTab.Focused || ((Control)this.CurrentTabPage.Tag).Focused)
                 this.Tag = this.ListTab.Tag;
             this.TabMenuControl(this.CurrentTabName);
@@ -6815,6 +6818,19 @@ namespace OpenTween
             return slbl.ToString();
         }
 
+        private void SubscribePrimaryAccountRatelimit()
+        {
+            this.unsubscribeRateLimitUpdate?.Dispose();
+            if (this.accounts.Primary is TwitterAccount twAccount)
+            {
+                var rateLimits = twAccount.AccountState.RateLimits;
+                this.unsubscribeRateLimitUpdate = rateLimits.SubscribeAccessLimitUpdated(this.TwitterApiStatus_AccessLimitUpdated);
+
+                // アカウントの切替を反映するため初回だけ空の更新通知を送る
+                this.TwitterApiStatus_AccessLimitUpdated(rateLimits, new(null));
+            }
+        }
+
         private async void TwitterApiStatus_AccessLimitUpdated(RateLimitCollection sender, RateLimitCollection.AccessLimitUpdatedEventArgs e)
         {
             try
@@ -6825,7 +6841,7 @@ namespace OpenTween
                 }
                 else
                 {
-                    this.SetApiStatusLabel(e.EndpointName);
+                    this.SetApiStatusLabel(sender, e.EndpointName);
                 }
             }
             catch (ObjectDisposedException)
@@ -6838,8 +6854,15 @@ namespace OpenTween
             }
         }
 
-        private void SetApiStatusLabel(string? endpointName = null)
+        private void SetApiStatusLabel(RateLimitCollection? rateLimits, string? endpointName = null)
         {
+            if (rateLimits == null)
+            {
+                this.toolStripApiGauge.ApiLimit = null;
+                this.toolStripApiGauge.ApiEndpoint = endpointName;
+                return;
+            }
+
             var tabType = this.CurrentTab.TabType;
 
             if (endpointName == null)
@@ -6870,7 +6893,7 @@ namespace OpenTween
                 };
             }
 
-            this.toolStripApiGauge.ApiLimit = endpointName != null ? MyCommon.TwitterRateLimits[endpointName] : null;
+            this.toolStripApiGauge.ApiLimit = endpointName != null ? rateLimits[endpointName] : null;
             this.toolStripApiGauge.ApiEndpoint = endpointName;
         }
 
@@ -8054,6 +8077,7 @@ namespace OpenTween
             }
 
             using var apiDlg = new ApiInfoDialog();
+            apiDlg.RateLimits = this.CurrentTabAccount is TwitterAccount twAccount ? twAccount.AccountState.RateLimits : null;
             apiDlg.ShowDialog(this);
         }
 

OpenTween.Tests/Connection/TwitterApiConnectionTest.cs
@@ -99,9 +99,10 @@ namespace OpenTween.Connection
         [Fact]
         public async Task SendAsync_UpdateRateLimitTest()
         {
+            var accountState = new TwitterAccountState();
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection();
+            using var apiConnection = new TwitterApiConnection(new TwitterCredentialNone(), accountState);
             apiConnection.Http = http;
 
             mockHandler.Enqueue(x =>
@@ -122,9 +123,6 @@ namespace OpenTween.Connection
                 };
             });
 
-            var rateLimits = new TwitterRateLimitCollection();
-            MyCommon.TwitterRateLimits = rateLimits;
-
             var request = new GetRequest
             {
                 RequestUri = new("hoge/tetete.json", UriKind.Relative),
@@ -133,7 +131,7 @@ namespace OpenTween.Connection
 
             using var response = await apiConnection.SendAsync(request);
 
-            Assert.Equal(new(150, 100, new DateTimeUtc(2013, 1, 1, 0, 0, 0)), rateLimits["/hoge/tetete"]);
+            Assert.Equal(new(150, 100, new DateTimeUtc(2013, 1, 1, 0, 0, 0)), accountState.RateLimits["/hoge/tetete"]);
 
             Assert.Equal(0, mockHandler.QueueCount);
         }

OpenTween/ApiInfoDialog.Designer.cs
@@ -91,7 +91,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "ApiInfoDialog";
             this.TopMost = true;
-            this.Shown += new System.EventHandler(this.ApiInfoDialog_Shown);
             this.ResumeLayout(false);
 
         }

OpenTween/ApiInfoDialog.cs
@@ -37,7 +37,21 @@ namespace OpenTween
 {
     public partial class ApiInfoDialog : OTBaseForm
     {
+        private RateLimitCollection? rateLimits;
         private IDisposable? unsubscribeRateLimitUpdate;
+
+        public RateLimitCollection? RateLimits
+        {
+            get => this.rateLimits;
+            set
+            {
+                if (this.rateLimits == value)
+                    return;
+
+                this.rateLimits = value;
+                this.InitializeRateLimits(value);
+            }
+        }
 
         public ApiInfoDialog()
             => this.InitializeComponent();
@@ -56,13 +70,19 @@ namespace OpenTween
             "/search/tweets",
         };
 
-        private void ApiInfoDialog_Shown(object sender, EventArgs e)
+        private void InitializeRateLimits(RateLimitCollection? rateLimits)
         {
+            this.ListViewApi.Items.Clear();
+            this.unsubscribeRateLimitUpdate?.Dispose();
+
+            if (rateLimits == null)
+                return;
+
             // TL更新用エンドポイントの追加
             var group = this.ListViewApi.Groups[0];
             foreach (var endpoint in this.tlEndpoints)
             {
-                var apiLimit = MyCommon.TwitterRateLimits[endpoint];
+                var apiLimit = rateLimits[endpoint];
                 if (apiLimit == null)
                     continue;
 
@@ -71,13 +91,13 @@ namespace OpenTween
 
             // その他
             group = this.ListViewApi.Groups[1];
-            var apiStatuses = MyCommon.TwitterRateLimits.Where(x => !this.tlEndpoints.Contains(x.Key)).OrderBy(x => x.Key);
+            var apiStatuses = rateLimits.Where(x => !this.tlEndpoints.Contains(x.Key)).OrderBy(x => x.Key);
             foreach (var (endpoint, apiLimit) in apiStatuses)
             {
                 this.AddListViewItem(endpoint, apiLimit, group);
             }
 
-            this.unsubscribeRateLimitUpdate = MyCommon.TwitterRateLimits.SubscribeAccessLimitUpdated(this.TwitterApiStatus_AccessLimitUpdated);
+            this.unsubscribeRateLimitUpdate = rateLimits.SubscribeAccessLimitUpdated(this.TwitterApiStatus_AccessLimitUpdated);
         }
 
         private void AddListViewItem(string endpoint, ApiLimit apiLimit, ListViewGroup group)
@@ -98,7 +118,7 @@ namespace OpenTween
 
         private void UpdateEndpointLimit(string endpoint)
         {
-            var apiLimit = MyCommon.TwitterRateLimits[endpoint];
+            var apiLimit = this.RateLimits?[endpoint];
             if (apiLimit != null)
             {
                 var item = this.ListViewApi.Items.Cast<ListViewItem>().Single(x => x.SubItems[0].Text == endpoint);

OpenTween/Connection/TwitterApiConnection.cs
@@ -101,7 +101,7 @@ namespace OpenTween.Connection
                 );
 
                 if (endpointName != null)
-                    MyCommon.TwitterRateLimits.UpdateFromHeader(responseMessage.Headers, endpointName);
+                    this.accountState.RateLimits.UpdateFromHeader(responseMessage.Headers, endpointName);
 
                 await TwitterApiConnection.CheckStatusCode(responseMessage, this.accountState)
                     .ConfigureAwait(false);
@@ -130,7 +130,7 @@ namespace OpenTween.Connection
         /// </summary>
         private void ThrowIfRateLimitExceeded(string endpointName)
         {
-            var limit = MyCommon.TwitterRateLimits[endpointName];
+            var limit = this.accountState.RateLimits[endpointName];
             if (limit == null)
                 return;
 

OpenTween/MyCommon.cs
@@ -662,8 +662,6 @@ namespace OpenTween
             SearchResults = 4096,
         }
 
-        public static TwitterRateLimitCollection TwitterRateLimits = new();
-
         public static bool IsAnimatedGif(string filename)
         {
             Image? img = null;

OpenTween/SocialProtocol/Twitter/TwitterAccountState.cs
@@ -51,6 +51,8 @@ namespace OpenTween.SocialProtocol.Twitter
 
         public TwitterTextConfiguration TextConfiguration { get; } = TwitterTextConfiguration.DefaultConfiguration();
 
+        public TwitterRateLimitCollection RateLimits { get; } = new();
+
         public bool HasUnrecoverableError { get; set; } = true;
 
         public TwitterAccountState()

OpenTween/Twitter.cs
@@ -190,7 +190,7 @@ namespace OpenTween
         }
 
         protected void ResetApiStatus()
-            => MyCommon.TwitterRateLimits.Clear();
+            => this.AccountState.RateLimits.Clear();
 
         public void ClearAuthInfo()
         {
@@ -765,9 +765,9 @@ namespace OpenTween
             var limits = await this.Api.ApplicationRateLimitStatus()
                 .ConfigureAwait(false);
 
-            MyCommon.TwitterRateLimits.UpdateFromJson(limits);
+            this.AccountState.RateLimits.UpdateFromJson(limits);
 
-            return MyCommon.TwitterRateLimits;
+            return this.AccountState.RateLimits;
         }
 
         /// <summary>

commit 4483f02f92e88020afd0228ff4f8506a1c3a452e

表示中のタブを切り替えるとレートリミットの情報が表示されなくなる不具合を修正
Fixes: f2ae9689 ("MyCommon.TwitterRateLimitsを廃止しアカウントごとにレートリミットを把握できるようにする")

OpenTween/Tween.cs
@@ -3056,7 +3056,7 @@ namespace OpenTween
         {
             this.SetMainWindowTitle();
             this.SetStatusLabelUrl();
-            this.SetApiStatusLabel(null);
+            this.SetApiStatusLabel(this.CurrentTabAccount.AccountState.RateLimits);
             if (this.ListTab.Focused || ((Control)this.CurrentTabPage.Tag).Focused)
                 this.Tag = this.ListTab.Tag;
             this.TabMenuControl(this.CurrentTabName);
@@ -6850,15 +6850,8 @@ namespace OpenTween
             }
         }
 
-        private void SetApiStatusLabel(RateLimitCollection? rateLimits, string? endpointName = null)
+        private void SetApiStatusLabel(RateLimitCollection rateLimits, string? endpointName = null)
         {
-            if (rateLimits == null)
-            {
-                this.toolStripApiGauge.ApiLimit = null;
-                this.toolStripApiGauge.ApiEndpoint = endpointName;
-                return;
-            }
-
             var tabType = this.CurrentTab.TabType;
 
             if (endpointName == null)