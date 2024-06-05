cdprf/OpenTween

commit 5232e94e331faf2176bbee5de7cdff65b43e592d

graphqlエンドポイントを使用したリストのタイムライン取得に対応

OpenTween/Api/GraphQL/TimelineTweet.cs
@@ -1,0 +1,143 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2023 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using OpenTween.Api.DataModel;
+
+namespace OpenTween.Api.GraphQL
+{
+    public class TimelineTweet
+    {
+        public const string TypeName = nameof(TimelineTweet);
+
+        public XElement Element { get; }
+
+        public TimelineTweet(XElement element)
+        {
+            var typeName = element.Element("itemType")?.Value;
+            if (typeName != TypeName)
+                throw new ArgumentException($"Invalid itemType: {typeName}", nameof(element));
+
+            this.Element = element;
+        }
+
+        public TwitterStatus ToTwitterStatus()
+        {
+            var tweetElm = this.Element.Element("tweet_results")?.Element("result") ?? throw CreateParseError();
+            return this.ParseTweet(tweetElm);
+        }
+
+        private TwitterStatus ParseTweet(XElement tweetElm)
+        {
+            var tweetLegacyElm = tweetElm.Element("legacy") ?? throw CreateParseError();
+            var userElm = tweetElm.Element("core")?.Element("user_results")?.Element("result") ?? throw CreateParseError();
+            var userLegacyElm = userElm.Element("legacy") ?? throw CreateParseError();
+            var retweetedTweetElm = tweetLegacyElm.Element("retweeted_status_result")?.Element("result");
+
+            static string GetText(XElement elm, string name)
+                => elm.Element(name)?.Value ?? throw CreateParseError();
+
+            static string? GetTextOrNull(XElement elm, string name)
+                => elm.Element(name)?.Value;
+
+            return new()
+            {
+                IdStr = GetText(tweetElm, "rest_id"),
+                Source = GetText(tweetElm, "source"),
+                CreatedAt = GetText(tweetLegacyElm, "created_at"),
+                FullText = GetText(tweetLegacyElm, "full_text"),
+                InReplyToScreenName = GetTextOrNull(tweetLegacyElm, "in_reply_to_screen_name"),
+                InReplyToStatusIdStr = GetTextOrNull(tweetLegacyElm, "in_reply_to_status_id_str"),
+                InReplyToUserId = GetTextOrNull(tweetLegacyElm, "in_reply_to_user_id_str") is string userId ? long.Parse(userId) : null,
+                Favorited = GetTextOrNull(tweetLegacyElm, "favorited") is string favorited ? favorited == "true" : null,
+                Entities = new()
+                {
+                    UserMentions = tweetLegacyElm.XPathSelectElements("entities/user_mentions/item")
+                        .Select(x => new TwitterEntityMention()
+                        {
+                            Indices = x.XPathSelectElements("indices/item").Select(x => int.Parse(x.Value)).ToArray(),
+                            ScreenName = GetText(x, "screen_name"),
+                        })
+                        .ToArray(),
+                    Urls = tweetLegacyElm.XPathSelectElements("entities/urls/item")
+                        .Select(x => new TwitterEntityUrl()
+                        {
+                            Indices = x.XPathSelectElements("indices/item").Select(x => int.Parse(x.Value)).ToArray(),
+                            DisplayUrl = GetText(x, "display_url"),
+                            ExpandedUrl = GetText(x, "expanded_url"),
+                            Url = GetText(x, "url"),
+                        })
+                        .ToArray(),
+                    Hashtags = tweetLegacyElm.XPathSelectElements("entities/hashtags/item")
+                        .Select(x => new TwitterEntityHashtag()
+                        {
+                            Indices = x.XPathSelectElements("indices/item").Select(x => int.Parse(x.Value)).ToArray(),
+                            Text = GetText(x, "text"),
+                        })
+                        .ToArray(),
+                },
+                ExtendedEntities = new()
+                {
+                    Media = tweetLegacyElm.XPathSelectElements("extended_entities/media/item")
+                        .Select(x => new TwitterEntityMedia()
+                        {
+                            Indices = x.XPathSelectElements("indices/item").Select(x => int.Parse(x.Value)).ToArray(),
+                            DisplayUrl = GetText(x, "display_url"),
+                            ExpandedUrl = GetText(x, "expanded_url"),
+                            Url = GetText(x, "url"),
+                            MediaUrlHttps = GetText(x, "media_url_https"),
+                            Type = GetText(x, "type"),
+                            AltText = GetTextOrNull(x, "ext_alt_text"),
+                        })
+                        .ToArray(),
+                },
+                User = new()
+                {
+                    Id = long.Parse(GetText(userElm, "rest_id")),
+                    IdStr = GetText(userElm, "rest_id"),
+                    Name = GetText(userLegacyElm, "name"),
+                    ProfileImageUrlHttps = GetText(userLegacyElm, "profile_image_url_https"),
+                    ScreenName = GetText(userLegacyElm, "screen_name"),
+                    Protected = GetTextOrNull(userLegacyElm, "protected") == "true",
+                },
+                RetweetedStatus = retweetedTweetElm != null ? this.ParseTweet(retweetedTweetElm) : null,
+            };
+        }
+
+        private static Exception CreateParseError()
+            => throw new WebApiException("Parse error on TimelineTweet");
+
+        public static TimelineTweet[] ExtractTimelineTweets(XElement element)
+        {
+            return element.XPathSelectElements($"//itemContent[itemType[text()='{TypeName}']][tweetDisplayType[text()='Tweet']]")
+                .Select(x => new TimelineTweet(x))
+                .ToArray();
+        }
+    }
+}

OpenTween.Tests/Api/GraphQL/ListLatestTweetsTimelineRequestTest.cs
@@ -1,0 +1,66 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2023 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using OpenTween.Api.TwitterV2;
+using OpenTween.Connection;
+using Xunit;
+
+namespace OpenTween.Api.GraphQL
+{
+    public class ListLatestTweetsTimelineRequestTest
+    {
+        [Fact]
+        public async Task Send_Test()
+        {
+            using var responseStream = File.OpenRead("Resources/Responses/ListLatestTweetsTimeline_SimpleTweet.json");
+
+            var mock = new Mock<IApiConnection>();
+            mock.Setup(x =>
+                    x.GetStreamAsync(It.IsAny<Uri>(), It.IsAny<IDictionary<string, string>>())
+                )
+                .Callback<Uri, IDictionary<string, string>>((url, param) =>
+                {
+                    Assert.Equal(new("https://twitter.com/i/api/graphql/6ClPnsuzQJ1p7-g32GQw9Q/ListLatestTweetsTimeline"), url);
+                    Assert.Equal(2, param.Count);
+                    Assert.Equal("""{"listId":"1675863884757110790","count":20}""", param["variables"]);
+                    Assert.True(param.ContainsKey("features"));
+                })
+                .ReturnsAsync(responseStream);
+
+            var request = new ListLatestTweetsTimelineRequest(listId: "1675863884757110790")
+            {
+                Count = 20,
+            };
+
+            var tweets = await request.Send(mock.Object).ConfigureAwait(false);
+            Assert.Single(tweets);
+
+            mock.VerifyAll();
+        }
+    }
+}

OpenTween.Tests/Api/GraphQL/TimelineTweetTest.cs
@@ -1,0 +1,114 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2023 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using OpenTween.Models;
+using Xunit;
+
+namespace OpenTween.Api.GraphQL
+{
+    public class TimelineTweetTest
+    {
+        private XElement LoadResponseDocument(string filename)
+        {
+            using var stream = File.OpenRead($"Resources/Responses/{filename}");
+            using var jsonReader = JsonReaderWriterFactory.CreateJsonReader(stream, XmlDictionaryReaderQuotas.Max);
+            return XElement.Load(jsonReader);
+        }
+
+        private TabInformations CreateTabInfo()
+        {
+            var tabinfo = new TabInformations();
+            tabinfo.AddDefaultTabs();
+            return tabinfo;
+        }
+
+        [Fact]
+        public void ExtractTimelineTweets_Single_Test()
+        {
+            var rootElm = this.LoadResponseDocument("ListLatestTweetsTimeline_SimpleTweet.json");
+            var timelineTweets = TimelineTweet.ExtractTimelineTweets(rootElm);
+            Assert.Single(timelineTweets);
+        }
+
+        [Fact]
+        public void ExtractTimelineTweets_Conversation_Test()
+        {
+            var rootElm = this.LoadResponseDocument("ListLatestTweetsTimeline_Conversation.json");
+            var timelineTweets = TimelineTweet.ExtractTimelineTweets(rootElm);
+            Assert.Equal(3, timelineTweets.Length);
+        }
+
+        [Fact]
+        public void ToStatus_WithTwitterPostFactory_SimpleTweet_Test()
+        {
+            var rootElm = this.LoadResponseDocument("TimelineTweet_SimpleTweet.json");
+            var timelineTweet = new TimelineTweet(rootElm);
+            var status = timelineTweet.ToTwitterStatus();
+            var postFactory = new TwitterPostFactory(this.CreateTabInfo());
+            var post = postFactory.CreateFromStatus(status, selfUserId: 1L, new HashSet<long>());
+
+            Assert.Equal("1613784711020826626", post.StatusId.Id);
+            Assert.Equal(40480664L, post.UserId);
+        }
+
+        [Fact]
+        public void ToStatus_WithTwitterPostFactory_TweetWithMedia_Test()
+        {
+            var rootElm = this.LoadResponseDocument("TimelineTweet_TweetWithMedia.json");
+            var timelineTweet = new TimelineTweet(rootElm);
+            var status = timelineTweet.ToTwitterStatus();
+            var postFactory = new TwitterPostFactory(this.CreateTabInfo());
+            var post = postFactory.CreateFromStatus(status, selfUserId: 1L, new HashSet<long>());
+
+            Assert.Equal("1614587968567783424", post.StatusId.Id);
+            Assert.Equal(40480664L, post.UserId);
+            Assert.Equal(2, post.Media.Count);
+            Assert.Equal("https://pbs.twimg.com/media/FmgrJiEaAAEU42G.png", post.Media[0].Url);
+            Assert.Equal("OpenTweenで @opentween のツイート一覧を表示しているスクショ", post.Media[0].AltText);
+            Assert.Equal("https://pbs.twimg.com/media/FmgrJiXaMAEu873.jpg", post.Media[1].Url);
+            Assert.Equal("OpenTweenの新しい画像投稿画面を動かしている様子のスクショ", post.Media[1].AltText);
+        }
+
+        [Fact]
+        public void ToStatus_WithTwitterPostFactory_RetweetedTweet_Test()
+        {
+            var rootElm = this.LoadResponseDocument("TimelineTweet_RetweetedTweet.json");
+            var timelineTweet = new TimelineTweet(rootElm);
+            var status = timelineTweet.ToTwitterStatus();
+            var postFactory = new TwitterPostFactory(this.CreateTabInfo());
+            var post = postFactory.CreateFromStatus(status, selfUserId: 1L, new HashSet<long>());
+
+            Assert.Equal("1617128268548964354", post.StatusId.Id);
+            Assert.Equal(40480664L, post.RetweetedByUserId);
+            Assert.Equal("1617126084138659840", post.RetweetedId!.Id);
+            Assert.Equal(514241801L, post.UserId);
+        }
+    }
+}

OpenTween.Tests/OpenTween.Tests.csproj
@@ -52,5 +52,20 @@
     <None Update="Resources\re1.png">
       <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
     </None>
+    <None Update="Resources\Responses\TimelineTweet_RetweetedTweet.json">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
+    <None Update="Resources\Responses\TimelineTweet_TweetWithMedia.json">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
+    <None Update="Resources\Responses\TimelineTweet_SimpleTweet.json">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
+    <None Update="Resources\Responses\ListLatestTweetsTimeline_Conversation.json">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
+    <None Update="Resources\Responses\ListLatestTweetsTimeline_SimpleTweet.json">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
   </ItemGroup>
 </Project>

OpenTween/Api/GraphQL/ListLatestTweetsTimelineRequest.cs
@@ -1,0 +1,90 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2023 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using OpenTween.Connection;
+
+namespace OpenTween.Api.GraphQL
+{
+    public class ListLatestTweetsTimelineRequest
+    {
+        private static readonly Uri EndpointUri = new("https://twitter.com/i/api/graphql/6ClPnsuzQJ1p7-g32GQw9Q/ListLatestTweetsTimeline");
+
+        public string ListId { get; set; }
+
+        public int Count { get; set; } = 20;
+
+        public ListLatestTweetsTimelineRequest(string listId)
+            => this.ListId = listId;
+
+        public Dictionary<string, string> CreateParameters()
+        {
+            return new()
+            {
+                ["variables"] = "{" +
+                    $@"""listId"":""{JsonUtils.EscapeJsonString(this.ListId)}""," +
+                    $@"""count"":{this.Count}" +
+                    "}",
+                ["features"] = "{" +
+                    @"""rweb_lists_timeline_redesign_enabled"":true," +
+                    @"""responsive_web_graphql_exclude_directive_enabled"":true," +
+                    @"""verified_phone_label_enabled"":false," +
+                    @"""creator_subscriptions_tweet_preview_api_enabled"":true," +
+                    @"""responsive_web_graphql_timeline_navigation_enabled"":true," +
+                    @"""responsive_web_graphql_skip_user_profile_image_extensions_enabled"":false," +
+                    @"""tweetypie_unmention_optimization_enabled"":true," +
+                    @"""responsive_web_edit_tweet_api_enabled"":true," +
+                    @"""graphql_is_translatable_rweb_tweet_is_translatable_enabled"":true," +
+                    @"""view_counts_everywhere_api_enabled"":true," +
+                    @"""longform_notetweets_consumption_enabled"":true," +
+                    @"""responsive_web_twitter_article_tweet_consumption_enabled"":false," +
+                    @"""tweet_awards_web_tipping_enabled"":false," +
+                    @"""freedom_of_speech_not_reach_fetch_enabled"":true," +
+                    @"""standardized_nudges_misinfo"":true," +
+                    @"""tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled"":true," +
+                    @"""longform_notetweets_rich_text_read_enabled"":true," +
+                    @"""longform_notetweets_inline_media_enabled"":true," +
+                    @"""responsive_web_media_download_video_enabled"":false," +
+                    @"""responsive_web_enhance_cards_enabled"":false" +
+                    "}",
+            };
+        }
+
+        public async Task<TimelineTweet[]> Send(IApiConnection apiConnection)
+        {
+            var param = this.CreateParameters();
+            using var stream = await apiConnection.GetStreamAsync(EndpointUri, param);
+            using var jsonReader = JsonReaderWriterFactory.CreateJsonReader(stream, XmlDictionaryReaderQuotas.Max);
+            var xElm = XElement.Load(jsonReader);
+
+            return TimelineTweet.ExtractTimelineTweets(xElm);
+        }
+    }
+}

OpenTween/Twitter.cs
@@ -43,6 +43,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenTween.Api;
 using OpenTween.Api.DataModel;
+using OpenTween.Api.GraphQL;
 using OpenTween.Api.TwitterV2;
 using OpenTween.Connection;
 using OpenTween.Models;
@@ -746,7 +747,17 @@ namespace OpenTween
             var count = GetApiResultCount(MyCommon.WORKERTYPE.List, more, startup);
 
             TwitterStatus[] statuses;
-            if (more)
+            if (this.Api.AppToken.AuthType == APIAuthType.TwitterComCookie)
+            {
+                var request = new ListLatestTweetsTimelineRequest(tab.ListInfo.Id.ToString())
+                {
+                    Count = count,
+                };
+                var timelineTweets = await request.Send(this.Api.Connection)
+                    .ConfigureAwait(false);
+                statuses = timelineTweets.Select(x => x.ToTwitterStatus()).ToArray();
+            }
+            else if (more)
             {
                 statuses = await this.Api.ListsStatuses(tab.ListInfo.Id, count, maxId: tab.OldestId, includeRTs: SettingManager.Instance.Common.IsListsIncludeRts)
                     .ConfigureAwait(false);

commit 3adf57dc3560f1a5fb213ca7c600734dada8ddea

TimelineTweetからの変換時にTwitterEntityMention.IdStrがnullになる不具合を修正
Fixes: 5232e94e ("graphqlエンドポイントを使用したリストのタイムライン取得に対応")

OpenTween/Api/GraphQL/TimelineTweet.cs
@@ -147,6 +147,7 @@ namespace OpenTween.Api.GraphQL
                         .Select(x => new TwitterEntityMention()
                         {
                             Indices = x.XPathSelectElements("indices/item").Select(x => int.Parse(x.Value)).ToArray(),
+                            IdStr = GetText(x, "id_str"),
                             ScreenName = GetText(x, "screen_name"),
                         })
                         .ToArray(),