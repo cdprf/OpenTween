cdprf/OpenTween

commit 2456ab12d7ab38d3f831f00edd866a1769ee4092

TwitterMediaIdをレコード型として追加

OpenTween/Twitter.cs
@@ -212,7 +212,7 @@ namespace OpenTween
 
             if (Twitter.DMSendTextRegex.IsMatch(param.Text))
             {
-                var mediaId = param.MediaIds != null && param.MediaIds.Any() ? param.MediaIds[0] : (long?)null;
+                var mediaId = param.MediaIds?.FirstOrDefault();
 
                 await this.SendDirectMessage(param.Text, mediaId)
                     .ConfigureAwait(false);
@@ -264,7 +264,7 @@ namespace OpenTween
             return post;
         }
 
-        public async Task<long> UploadMedia(IMediaItem item, string? mediaCategory = null)
+        public async Task<TwitterMediaId> UploadMedia(IMediaItem item, string? mediaCategory = null)
         {
             this.CheckAccountState();
 
@@ -283,7 +283,7 @@ namespace OpenTween
             var initMedia = await initResponse.LoadJsonAsync()
                 .ConfigureAwait(false);
 
-            var mediaId = initMedia.MediaId;
+            var mediaId = new TwitterMediaId(initMedia.MediaIdStr);
 
             await this.Api.MediaUploadAppend(mediaId, 0, item)
                 .ConfigureAwait(false);
@@ -318,10 +318,10 @@ namespace OpenTween
             }
 
             succeeded:
-            return media.MediaId;
+            return mediaId;
         }
 
-        public async Task SendDirectMessage(string postStr, long? mediaId = null)
+        public async Task SendDirectMessage(string postStr, TwitterMediaId? mediaId = null)
         {
             this.CheckAccountState();
 

OpenTween.Tests/Api/TwitterApiTest.cs
@@ -33,6 +33,7 @@ using Moq;
 using OpenTween.Api.DataModel;
 using OpenTween.Connection;
 using OpenTween.Models;
+using OpenTween.SocialProtocol.Twitter;
 using Xunit;
 
 namespace OpenTween.Api
@@ -275,7 +276,7 @@ namespace OpenTween.Api
             await twitterApi.StatusesUpdate(
                     "hogehoge",
                     replyToId: new("100"),
-                    mediaIds: new[] { 10L, 20L },
+                    mediaIds: new TwitterMediaId[] { new("10"), new("20") },
                     autoPopulateReplyMetadata: true,
                     excludeReplyUserIds: new TwitterUserId[] { new("100"), new("200") },
                     attachmentUrl: "https://twitter.com/twitterapi/status/22634515958"
@@ -771,7 +772,7 @@ namespace OpenTween.Api
             using var twitterApi = new TwitterApi();
             twitterApi.ApiConnection = mock.Object;
 
-            await twitterApi.DirectMessagesEventsNew(recipientId: new("12345"), text: "hogehoge", mediaId: 67890L);
+            await twitterApi.DirectMessagesEventsNew(recipientId: new("12345"), text: "hogehoge", mediaId: new("67890"));
 
             mock.VerifyAll();
         }
@@ -1348,7 +1349,7 @@ namespace OpenTween.Api
             using var twitterApi = new TwitterApi();
             twitterApi.ApiConnection = mock.Object;
 
-            await twitterApi.MediaUploadAppend(mediaId: 11111L, segmentIndex: 1, media: media);
+            await twitterApi.MediaUploadAppend(mediaId: new("11111"), segmentIndex: 1, media: media);
 
             mock.VerifyAll();
         }
@@ -1370,7 +1371,7 @@ namespace OpenTween.Api
             using var twitterApi = new TwitterApi();
             twitterApi.ApiConnection = mock.Object;
 
-            await twitterApi.MediaUploadFinalize(mediaId: 11111L)
+            await twitterApi.MediaUploadFinalize(mediaId: new("11111"))
                 .IgnoreResponse();
 
             mock.VerifyAll();
@@ -1396,7 +1397,7 @@ namespace OpenTween.Api
             using var twitterApi = new TwitterApi();
             twitterApi.ApiConnection = mock.Object;
 
-            await twitterApi.MediaUploadStatus(mediaId: 11111L);
+            await twitterApi.MediaUploadStatus(mediaId: new("11111"));
 
             mock.VerifyAll();
         }
@@ -1413,7 +1414,7 @@ namespace OpenTween.Api
             using var twitterApi = new TwitterApi();
             twitterApi.ApiConnection = mock.Object;
 
-            await twitterApi.MediaMetadataCreate(mediaId: 12345L, altText: "hogehoge");
+            await twitterApi.MediaMetadataCreate(mediaId: new("12345"), altText: "hogehoge");
 
             mock.VerifyAll();
         }

OpenTween.Tests/SocialProtocol/Twitter/CreateTweetFormatterTest.cs
@@ -165,11 +165,11 @@ namespace OpenTween.SocialProtocol.Twitter
             // 引用ツイートと画像添付は併用できないため attachment_url は使用しない（現在は許容されているかも？）
             var postParams = new PostStatusParams(Text: "hoge https://twitter.com/twitterapi/status/22634515958")
             {
-                MediaIds = new[] { 1234L },
+                MediaIds = new[] { new TwitterMediaId("1234") },
             };
             var expected = new CreateTweetParams(Text: "hoge https://twitter.com/twitterapi/status/22634515958")
             {
-                MediaIds = new[] { 1234L },
+                MediaIds = new[] { new TwitterMediaId("1234") },
             };
             Assert.Equal(expected, formatter.CreateParams(postParams));
         }

OpenTween/Api/TwitterApi.cs
@@ -32,6 +32,7 @@ using System.Threading.Tasks;
 using OpenTween.Api.DataModel;
 using OpenTween.Connection;
 using OpenTween.Models;
+using OpenTween.SocialProtocol.Twitter;
 
 namespace OpenTween.Api
 {
@@ -191,7 +192,7 @@ namespace OpenTween.Api
         public async Task<LazyJson<TwitterStatus>> StatusesUpdate(
             string status,
             TwitterStatusId? replyToId,
-            IReadOnlyList<long>? mediaIds,
+            IReadOnlyList<TwitterMediaId>? mediaIds,
             bool? autoPopulateReplyMetadata = null,
             IReadOnlyList<TwitterUserId>? excludeReplyUserIds = null,
             string? attachmentUrl = null)
@@ -207,7 +208,7 @@ namespace OpenTween.Api
             if (replyToId != null)
                 param["in_reply_to_status_id"] = replyToId.Id;
             if (mediaIds != null && mediaIds.Count > 0)
-                param.Add("media_ids", string.Join(",", mediaIds));
+                param.Add("media_ids", string.Join(",", mediaIds.Select(x => x.Id)));
             if (autoPopulateReplyMetadata != null)
                 param["auto_populate_reply_metadata"] = autoPopulateReplyMetadata.Value ? "true" : "false";
             if (excludeReplyUserIds != null && excludeReplyUserIds.Count > 0)
@@ -613,7 +614,7 @@ namespace OpenTween.Api
                 .ConfigureAwait(false);
         }
 
-        public async Task<LazyJson<TwitterMessageEventSingle>> DirectMessagesEventsNew(TwitterUserId recipientId, string text, long? mediaId = null)
+        public async Task<LazyJson<TwitterMessageEventSingle>> DirectMessagesEventsNew(TwitterUserId recipientId, string text, TwitterMediaId? mediaId = null)
         {
             var attachment = "";
             if (mediaId != null)
@@ -622,7 +623,7 @@ namespace OpenTween.Api
                             "attachment": {
                               "type": "media",
                               "media": {
-                                "id": "{{JsonUtils.EscapeJsonString(mediaId.ToString())}}"
+                                "id": "{{JsonUtils.EscapeJsonString(mediaId.Id)}}"
                               }
                             }
                     """;
@@ -1106,7 +1107,7 @@ namespace OpenTween.Api
             return response.ReadAsLazyJson<TwitterUploadMediaInit>();
         }
 
-        public async Task MediaUploadAppend(long mediaId, int segmentIndex, IMediaItem media)
+        public async Task MediaUploadAppend(TwitterMediaId mediaId, int segmentIndex, IMediaItem media)
         {
             var request = new PostMultipartRequest
             {
@@ -1114,7 +1115,7 @@ namespace OpenTween.Api
                 Query = new Dictionary<string, string>
                 {
                     ["command"] = "APPEND",
-                    ["media_id"] = mediaId.ToString(),
+                    ["media_id"] = mediaId.Id,
                     ["segment_index"] = segmentIndex.ToString(),
                 },
                 Media = new Dictionary<string, IMediaItem>
@@ -1128,7 +1129,7 @@ namespace OpenTween.Api
                 .ConfigureAwait(false);
         }
 
-        public async Task<LazyJson<TwitterUploadMediaResult>> MediaUploadFinalize(long mediaId)
+        public async Task<LazyJson<TwitterUploadMediaResult>> MediaUploadFinalize(TwitterMediaId mediaId)
         {
             var request = new PostRequest
             {
@@ -1136,7 +1137,7 @@ namespace OpenTween.Api
                 Query = new Dictionary<string, string>
                 {
                     ["command"] = "FINALIZE",
-                    ["media_id"] = mediaId.ToString(),
+                    ["media_id"] = mediaId.Id,
                 },
             };
 
@@ -1146,7 +1147,7 @@ namespace OpenTween.Api
             return response.ReadAsLazyJson<TwitterUploadMediaResult>();
         }
 
-        public async Task<TwitterUploadMediaResult> MediaUploadStatus(long mediaId)
+        public async Task<TwitterUploadMediaResult> MediaUploadStatus(TwitterMediaId mediaId)
         {
             var request = new GetRequest
             {
@@ -1154,7 +1155,7 @@ namespace OpenTween.Api
                 Query = new Dictionary<string, string>
                 {
                     ["command"] = "STATUS",
-                    ["media_id"] = mediaId.ToString(),
+                    ["media_id"] = mediaId.Id,
                 },
             };
 
@@ -1165,13 +1166,14 @@ namespace OpenTween.Api
                 .ConfigureAwait(false);
         }
 
-        public async Task MediaMetadataCreate(long mediaId, string altText)
+        public async Task MediaMetadataCreate(TwitterMediaId mediaId, string altText)
         {
+            var escapedMediaId = JsonUtils.EscapeJsonString(mediaId.Id);
             var escapedAltText = JsonUtils.EscapeJsonString(altText);
             var request = new PostJsonRequest
             {
                 RequestUri = new("https://upload.twitter.com/1.1/media/metadata/create.json"),
-                JsonString = $$$"""{"media_id": "{{{mediaId}}}", "alt_text": {"text": "{{{escapedAltText}}}"}}""",
+                JsonString = $$$"""{"media_id": "{{{escapedMediaId}}}", "alt_text": {"text": "{{{escapedAltText}}}"}}""",
             };
 
             await this.Connection.SendAsync(request)

OpenTween/MediaUploadServices/TwitterPhoto.cs
@@ -36,6 +36,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using OpenTween.Api.DataModel;
 using OpenTween.Setting;
+using OpenTween.SocialProtocol.Twitter;
 
 namespace OpenTween.MediaUploadServices
 {
@@ -89,7 +90,7 @@ namespace OpenTween.MediaUploadServices
                     throw new ArgumentException("Err:Media not found.");
             }
 
-            long[] mediaIds;
+            TwitterMediaId[] mediaIds;
 
             if (Twitter.DMSendTextRegex.IsMatch(postParams.Text))
                 mediaIds = new[] { await this.UploadMediaForDM(mediaItems).ConfigureAwait(false) };
@@ -106,7 +107,7 @@ namespace OpenTween.MediaUploadServices
         public void UpdateTwitterConfiguration(TwitterConfiguration config)
             => this.twitterConfig = config;
 
-        private async Task<long[]> UploadMediaForTweet(IMediaItem[] mediaItems)
+        private async Task<TwitterMediaId[]> UploadMediaForTweet(IMediaItem[] mediaItems)
         {
             var uploadTasks = from m in mediaItems
                               select this.UploadMediaItem(m, mediaCategory: null);
@@ -117,7 +118,7 @@ namespace OpenTween.MediaUploadServices
             return mediaIds;
         }
 
-        private async Task<long> UploadMediaForDM(IMediaItem[] mediaItems)
+        private async Task<TwitterMediaId> UploadMediaForDM(IMediaItem[] mediaItems)
         {
             if (mediaItems.Length > 1)
                 throw new InvalidOperationException("Err:Can't attach multiple media to DM.");
@@ -135,9 +136,9 @@ namespace OpenTween.MediaUploadServices
             return mediaId;
         }
 
-        private async Task<long> UploadMediaItem(IMediaItem mediaItem, string? mediaCategory)
+        private async Task<TwitterMediaId> UploadMediaItem(IMediaItem mediaItem, string? mediaCategory)
         {
-            async Task<long> UploadInternal(IMediaItem media, string? category)
+            async Task<TwitterMediaId> UploadInternal(IMediaItem media, string? category)
             {
                 var mediaId = await this.tw.UploadMedia(media, category)
                     .ConfigureAwait(false);

OpenTween/Models/INativeUploadMediaId.cs
@@ -1,5 +1,5 @@
 ﻿// OpenTween - Client of Twitter
-// Copyright (c) 2016 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// Copyright (c) 2024 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
 // All rights reserved.
 //
 // This file is part of OpenTween.
@@ -21,18 +21,10 @@
 
 #nullable enable
 
-using System;
-using System.Collections.Generic;
-using OpenTween.Models;
-
-namespace OpenTween
+namespace OpenTween.Models
 {
-    public record PostStatusParams(
-        string Text,
-        PostClass? InReplyTo = null,
-        IReadOnlyList<INativeUploadMediaId>? MediaIds = null
-    )
+    public interface INativeUploadMediaId
     {
-        public IReadOnlyList<INativeUploadMediaId> MediaIds { get; init; } = MediaIds ?? Array.Empty<INativeUploadMediaId>();
+        public string Id { get; }
     }
 }

OpenTween/SocialProtocol/Twitter/CreateTweetFormatter.cs
@@ -52,7 +52,7 @@ namespace OpenTween.SocialProtocol.Twitter
             var createParams = new CreateTweetParams(
                 formState.Text,
                 formState.InReplyTo,
-                formState.MediaIds
+                formState.MediaIds.Cast<TwitterMediaId>().ToArray()
             );
 
             // DM の場合はこれ以降の処理を行わない

OpenTween/SocialProtocol/Twitter/CreateTweetParams.cs
@@ -31,13 +31,13 @@ namespace OpenTween.SocialProtocol.Twitter
     public record CreateTweetParams(
         string Text,
         PostClass? InReplyTo = null,
-        IReadOnlyList<long>? MediaIds = null,
+        IReadOnlyList<TwitterMediaId>? MediaIds = null,
         bool AutoPopulateReplyMetadata = false,
         IReadOnlyList<PersonId>? ExcludeReplyUserIds = null,
         string? AttachmentUrl = null
     )
     {
-        public IReadOnlyList<long> MediaIds { get; init; } = MediaIds ?? Array.Empty<long>();
+        public IReadOnlyList<TwitterMediaId> MediaIds { get; init; } = MediaIds ?? Array.Empty<TwitterMediaId>();
 
         public IReadOnlyList<PersonId> ExcludeReplyUserIds { get; init; } = ExcludeReplyUserIds ?? Array.Empty<PersonId>();
 

commit 8e0a8469c15689d6dc2120a418be0c374f0a3352

画像投稿時にTwitterMediaIdからstringへの変換が正しく行えてない不具合を修正
Fixes: 2456ab12 ("TwitterMediaIdをレコード型として追加")

OpenTween/Api/GraphQL/CreateTweetRequest.cs
@@ -30,6 +30,7 @@ using System.Xml.XPath;
 using OpenTween.Api.DataModel;
 using OpenTween.Connection;
 using OpenTween.Models;
+using OpenTween.SocialProtocol.Twitter;
 
 namespace OpenTween.Api.GraphQL
 {
@@ -43,7 +44,7 @@ namespace OpenTween.Api.GraphQL
 
         public TwitterUserId[] ExcludeReplyUserIds { get; set; } = Array.Empty<TwitterUserId>();
 
-        public string[] MediaIds { get; set; } = Array.Empty<string>();
+        public TwitterMediaId[] MediaIds { get; set; } = Array.Empty<TwitterMediaId>();
 
         public string? AttachmentUrl { get; set; }
 
@@ -112,7 +113,7 @@ namespace OpenTween.Api.GraphQL
                         ? new(
                             MediaEntities: this.MediaIds
                                 .Select(x => new VariableMediaEntity(
-                                    MediaId: x,
+                                    MediaId: x.Id,
                                     TaggedUsers: Array.Empty<string>()
                                 ))
                                 .ToArray(),

OpenTween/Twitter.cs
@@ -228,7 +228,7 @@ namespace OpenTween
                     TweetText = param.Text,
                     InReplyToTweetId = param.InReplyTo?.StatusId.ToTwitterStatusId(),
                     ExcludeReplyUserIds = param.ExcludeReplyUserIds.OfType<TwitterUserId>().ToArray(),
-                    MediaIds = param.MediaIds.Select(x => x.ToString()).ToArray(),
+                    MediaIds = param.MediaIds.ToArray(),
                     AttachmentUrl = param.AttachmentUrl,
                 };
 

OpenTween.Tests/Api/GraphQL/CreateTweetRequestTest.cs
@@ -23,6 +23,7 @@ using System.Threading.Tasks;
 using Moq;
 using OpenTween.Connection;
 using OpenTween.Models;
+using OpenTween.SocialProtocol.Twitter;
 using Xunit;
 
 namespace OpenTween.Api.GraphQL
@@ -104,7 +105,7 @@ namespace OpenTween.Api.GraphQL
             var request = new CreateTweetRequest
             {
                 TweetText = "tetete",
-                MediaIds = new[] { "11111", "22222" },
+                MediaIds = new TwitterMediaId[] { new("11111"), new("22222") },
             };
             await request.Send(mock.Object);
             mock.VerifyAll();