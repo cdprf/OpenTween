cdprf/OpenTween

commit eb7dd7fd3f6ffeef4086dbfd9c3bb3a6de806305

TwitterAccountStateに FollowerIds, NoRetweetUserIds を追加

OpenTween/SocialProtocol/Twitter/TwitterAccountState.cs
@@ -21,6 +21,7 @@
 
 #nullable enable
 
+using System.Collections.Generic;
 using OpenTween.Api.DataModel;
 
 namespace OpenTween.SocialProtocol.Twitter
@@ -36,6 +37,10 @@ namespace OpenTween.SocialProtocol.Twitter
         public int? FriendsCount { get; private set; }
 
         public int? StatusesCount { get; private set; }
+
+        public ISet<long> FollowerIds { get; set; } = new HashSet<long>();
+
+        public ISet<long> NoRetweetUserIds { get; set; } = new HashSet<long>();
 
         public TwitterAccountState()
             : this(0L, "")

OpenTween/Twitter.cs
@@ -171,8 +171,6 @@ namespace OpenTween
         private delegate void GetIconImageDelegate(PostClass post);
 
         private readonly object lockObj = new();
-        private ISet<long> followerId = new HashSet<long>();
-        private long[] noRTId = Array.Empty<long>();
 
         private readonly TwitterPostFactory postFactory;
         private readonly PostUrlExpander urlExpander;
@@ -826,7 +824,7 @@ namespace OpenTween
 
         private PostClass CreatePostsFromStatusData(TwitterStatus status, bool firstLoad, bool favTweet)
         {
-            var post = this.postFactory.CreateFromStatus(status, this.UserId, this.followerId, firstLoad, favTweet);
+            var post = this.postFactory.CreateFromStatus(status, this.UserId, this.Api.AccountState.FollowerIds, firstLoad, favTweet);
             _ = this.urlExpander.Expand(post);
 
             return post;
@@ -842,7 +840,7 @@ namespace OpenTween
         }
 
         private PostClass[] FilterNoRetweetUserPosts(PostClass[] posts)
-            => posts.Where(x => x.RetweetedByUserId == null || !this.noRTId.Contains(x.RetweetedByUserId.Value)).ToArray();
+            => posts.Where(x => x.RetweetedByUserId == null || !this.Api.AccountState.NoRetweetUserIds.Contains(x.RetweetedByUserId.Value)).ToArray();
 
         public async Task GetListStatus(ListTimelineTabModel tab, bool more, bool firstLoad)
         {
@@ -1287,8 +1285,8 @@ namespace OpenTween
             }
             while (cursor != 0);
 
-            this.followerId = newFollowerIds.ToHashSet();
-            TabInformations.GetInstance().RefreshOwl(this.followerId);
+            this.Api.AccountState.FollowerIds = newFollowerIds.ToHashSet();
+            TabInformations.GetInstance().RefreshOwl(this.Api.AccountState.FollowerIds);
 
             this.GetFollowersSuccess = true;
         }
@@ -1301,9 +1299,10 @@ namespace OpenTween
         {
             if (MyCommon.EndingFlag) return;
 
-            this.noRTId = await this.Api.NoRetweetIds()
+            var noRetweetUserIds = await this.Api.NoRetweetIds()
                 .ConfigureAwait(false);
 
+            this.Api.AccountState.NoRetweetUserIds = new HashSet<long>(noRetweetUserIds);
             this.GetNoRetweetSuccess = true;
         }
 

commit 398afe4f10b1516abc09b1b17202cd103f3a848e

設定画面を閉じる度にFollowerIdsなどが空の状態にリセットされる不具合を修正
Fixes: eb7dd7fd ("TwitterAccountStateに FollowerIds, NoRetweetUserIds を追加")

OpenTween/SocialProtocol/Misskey/MisskeyAccount.cs
@@ -42,7 +42,7 @@ namespace OpenTween.SocialProtocol.Misskey
 
         public bool IsDisposed { get; private set; }
 
-        public MisskeyAccountState AccountState { get; private set; } = new();
+        public MisskeyAccountState AccountState { get; } = new();
 
         ISocialAccountState ISocialAccount.AccountState
             => this.AccountState;
@@ -71,14 +71,10 @@ namespace OpenTween.SocialProtocol.Misskey
         {
             Debug.Assert(accountSettings.UniqueKey == this.UniqueKey.Id, "UniqueKey must be same as current value.");
 
-            var serverUri = new Uri($"https://{accountSettings.ServerHostname}/");
-            this.AccountState = new(serverUri, new(accountSettings.UserId), accountSettings.Username)
-            {
-                AuthorizedScopes = accountSettings.Scopes,
-                HasUnrecoverableError = false,
-            };
+            this.AccountState.UpdateFromSettings(accountSettings);
+            this.AccountState.HasUnrecoverableError = false;
 
-            var apiBaseUri = new Uri(serverUri, "/api/");
+            var apiBaseUri = new Uri(this.AccountState.ServerUri, "/api/");
 
             var newConnection = new MisskeyApiConnection(apiBaseUri, accountSettings.TokenSecret, this.AccountState);
             (this.connection, var oldConnection) = (newConnection, this.connection);

OpenTween/SocialProtocol/Misskey/MisskeyAccountState.cs
@@ -68,6 +68,14 @@ namespace OpenTween.SocialProtocol.Misskey
             this.UserName = userName;
         }
 
+        public void UpdateFromSettings(UserAccount accountSettings)
+        {
+            this.ServerUri = new($"https://{accountSettings.ServerHostname}/");
+            this.UserId = new(accountSettings.UserId);
+            this.UserName = accountSettings.Username;
+            this.AuthorizedScopes = accountSettings.Scopes;
+        }
+
         /// <summary>ユーザー情報を更新します</summary>
         public void UpdateFromUser(MisskeyUser self)
         {

OpenTween/SocialProtocol/Twitter/TwitterAccount.cs
@@ -41,7 +41,7 @@ namespace OpenTween.SocialProtocol.Twitter
 
         public bool IsDisposed { get; private set; }
 
-        public TwitterAccountState AccountState { get; private set; } = new();
+        public TwitterAccountState AccountState { get; } = new();
 
         ISocialAccountState ISocialAccount.AccountState
             => this.AccountState;
@@ -72,12 +72,9 @@ namespace OpenTween.SocialProtocol.Twitter
             Debug.Assert(accountSettings.UniqueKey == this.UniqueKey.Id, "UniqueKey must be same as current value.");
 
             var credential = accountSettings.GetTwitterCredential();
-            var userId = new TwitterUserId(accountSettings.UserId);
 
-            this.AccountState = new TwitterAccountState(userId, accountSettings.Username)
-            {
-                HasUnrecoverableError = credential is TwitterCredentialNone,
-            };
+            this.AccountState.UpdateFromSettings(accountSettings);
+            this.AccountState.HasUnrecoverableError = credential is TwitterCredentialNone;
 
             var newConnection = new TwitterApiConnection(credential, this.AccountState);
             (this.apiConnection, var oldConnection) = (newConnection, this.apiConnection);

OpenTween/SocialProtocol/Twitter/TwitterAccountState.cs
@@ -73,6 +73,12 @@ namespace OpenTween.SocialProtocol.Twitter
             this.UserName = userName;
         }
 
+        public void UpdateFromSettings(UserAccount accountSettings)
+        {
+            this.UserId = new(accountSettings.UserId);
+            this.UserName = accountSettings.Username;
+        }
+
         /// <summary>ユーザー情報を更新します</summary>
         public void UpdateFromUser(TwitterUser self)
         {